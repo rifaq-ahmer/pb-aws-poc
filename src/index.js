8ee882fe2479899bddaf9f2c4f880746eac7049e
Rifaq.Shaikh <rifaq.shaikh@extentia.com>
2021-10-06 16:17:00 +0530

    added authenticationFlowType

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ Amplify.configure({
 		userPoolId: config.cognito.USER_POOL_ID,
 		userPoolWebClientId: config.cognito.APP_CLIENT_ID,
 	},
-	// authenticationFlowType: "USER_PASSWORD_AUTH",
+	authenticationFlowType: "ALLOW_USER_SRP_AUTH",
 	oauth: {
 		domain: "https://applicants.auth.ap-south-1.amazoncognito.com",
 		scope: [
