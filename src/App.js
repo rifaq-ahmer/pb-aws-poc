c6b11d2fa7fa558623c955128f95d8ec09312785
Rifaq.Shaikh <rifaq.shaikh@extentia.com>
2021-10-12 12:29:34 +0530

    added amplify.configure obj

--- src/App.js
+++ src/App.js
@@ -16,37 +16,65 @@ import { config } from "./aws-config";
 import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
 
 Amplify.configure({
-	Auth: {
-		mandatorySignIn: true,
-		region: config.cognito.REGION,
-		userPoolId: config.cognito.USER_POOL_ID,
-		userPoolWebClientId: config.cognito.APP_CLIENT_ID,
-	},
-	authenticationFlowType: "USER_SRP_AUTH",
-
-	oauth: {
-		domain: "applicants.auth.ap-south-1.amazoncognito.com",
-
-		scope: ["email", "openid"],
-
-		redirectSignIn: "https://master.d3n4gc65xuvvqw.amplifyapp.com/",
-
-		logoutUri: "https://master.d3n4gc65xuvvqw.amplifyapp.com/",
-
-		redirectUri: "https://master.d3n4gc65xuvvqw.amplifyapp.com/",
-
-		redirectSignOut: "https://master.d3n4gc65xuvvqw.amplifyapp.com/",
-
-		responseType: "token",
-	},
 	API: {
 		endpoints: [
 			{
-				name: "Popular Bank",
-				endpoint: config.apiGateway.URL,
-				region: config.apiGateway.REGION,
+				name: "ApplicantSubmission",
+
+				endpoint:
+					"https://g9yh14f7ve.execute-api.ap-south-1.amazonaws.com/Authorizeddev",
 			},
 		],
+	},
+
+	Auth: {
+		// REQUIRED - Amazon Cognito Region
+
+		region: "ap-south-1",
+
+		// OPTIONAL - Amazon Cognito Federated Identity Pool Region
+
+		// Required only if it's different from Amazon Cognito Region
+
+		identityPoolRegion: "ap-south-1",
+
+		// OPTIONAL - Amazon Cognito User Pool ID
+
+		userPoolId: "ap-south-1_T6Eai4D7M",
+
+		// OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+
+		userPoolWebClientId: "5hesaar1ubosqmd96f6kucqjiu",
+
+		// OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+
+		mandatorySignIn: false,
+
+		// OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
+
+		authenticationFlowType: "USER_SRP_AUTH",
+
+		// OPTIONAL - Manually set key value pairs that can be passed to Cognito Lambda Triggers
+
+		clientMetadata: { myCustomKey: "myCustomValue" },
+
+		// OPTIONAL - Hosted UI configuration
+
+		oauth: {
+			domain: "applicants.auth.ap-south-1.amazoncognito.com",
+
+			scope: ["email", "openid"],
+
+			redirectSignIn: "https://master.d3n4gc65xuvvqw.amplifyapp.com/",
+
+			logoutUri: "https://master.d3n4gc65xuvvqw.amplifyapp.com/",
+
+			redirectUri: "https://master.d3n4gc65xuvvqw.amplifyapp.com/",
+
+			redirectSignOut: "https://master.d3n4gc65xuvvqw.amplifyapp.com/",
+
+			responseType: "token", // or 'token', note that REFRESH token will only be generated when the responseType is code
+		},
 	},
 });
 
@@ -148,3 +176,39 @@ function App() {
 }
 
 export default withAuthenticator(App);
+
+// {
+// 	Auth: {
+// 		mandatorySignIn: true,
+// 		region: config.cognito.REGION,
+// 		userPoolId: config.cognito.USER_POOL_ID,
+// 		userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+// 	},
+// 	authenticationFlowType: "USER_SRP_AUTH",
+// 	clientMetadata: { myCustomKey: "myCustomValue" },
+
+// 	oauth: {
+// 		domain: "applicants.auth.ap-south-1.amazoncognito.com",
+
+// 		scope: ["email", "openid"],
+
+// 		redirectSignIn: "https://master.d3n4gc65xuvvqw.amplifyapp.com/",
+
+// 		logoutUri: "https://master.d3n4gc65xuvvqw.amplifyapp.com/",
+
+// 		redirectUri: "https://master.d3n4gc65xuvvqw.amplifyapp.com/",
+
+// 		redirectSignOut: "https://master.d3n4gc65xuvvqw.amplifyapp.com/",
+
+// 		responseType: "token",
+// 	},
+// 	API: {
+// 		endpoints: [
+// 			{
+// 				name: "Popular Bank",
+// 				endpoint: config.apiGateway.URL,
+// 				region: config.apiGateway.REGION,
+// 			},
+// 		],
+// 	},
+// }
