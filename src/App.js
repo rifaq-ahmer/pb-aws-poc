d8dd64a416599c67cc867ce02f94b0a35895f63e
Rifaq.Shaikh <rifaq.shaikh@extentia.com>
2021-10-12 13:04:33 +0530

    added amplify-config

--- src/App.js
+++ src/App.js
@@ -12,71 +12,10 @@ import AppliacantDetails from "./Pages/ViewApplicantsDetails/viewApplicantDetail
 import ViewBuisnessDetails from "./Pages/ViewBuisnessDetails/viewBuisnessDetails";
 import ViewLoanDetails from "./Pages/ViewLoanDetails/viewLoanDetails";
 import Amplify from "aws-amplify";
-import { config } from "./aws-config";
+import { Amplifyconfig, config } from "./aws-config";
 import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
 
-Amplify.configure({
-	API: {
-		endpoints: [
-			{
-				name: "ApplicantSubmission",
-
-				endpoint:
-					"https://g9yh14f7ve.execute-api.ap-south-1.amazonaws.com/Authorizeddev",
-			},
-		],
-	},
-
-	Auth: {
-		// REQUIRED - Amazon Cognito Region
-
-		region: "ap-south-1",
-
-		// OPTIONAL - Amazon Cognito Federated Identity Pool Region
-
-		// Required only if it's different from Amazon Cognito Region
-
-		identityPoolRegion: "ap-south-1",
-
-		// OPTIONAL - Amazon Cognito User Pool ID
-
-		userPoolId: "ap-south-1_T6Eai4D7M",
-
-		// OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-
-		userPoolWebClientId: "5hesaar1ubosqmd96f6kucqjiu",
-
-		// OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
-
-		mandatorySignIn: false,
-
-		// OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
-
-		authenticationFlowType: "USER_SRP_AUTH",
-
-		// OPTIONAL - Manually set key value pairs that can be passed to Cognito Lambda Triggers
-
-		clientMetadata: { myCustomKey: "myCustomValue" },
-
-		// OPTIONAL - Hosted UI configuration
-
-		oauth: {
-			domain: "applicants.auth.ap-south-1.amazoncognito.com",
-
-			scope: ["email", "openid"],
-
-			redirectSignIn: "https://master.d3n4gc65xuvvqw.amplifyapp.com/",
-
-			logoutUri: "https://master.d3n4gc65xuvvqw.amplifyapp.com/",
-
-			redirectUri: "https://master.d3n4gc65xuvvqw.amplifyapp.com/",
-
-			redirectSignOut: "https://master.d3n4gc65xuvvqw.amplifyapp.com/",
-
-			responseType: "token", // or 'token', note that REFRESH token will only be generated when the responseType is code
-		},
-	},
-});
+Amplify.configure(Amplifyconfig);
 
 function App() {
 	const [applicantDetails, setApplicantDetails] = useState([]);
@@ -102,7 +41,7 @@ function App() {
 	const userData = localStorage.getItem(
 		`CognitoIdentityServiceProvider.${APP_CLIENT_ID}.${userName}.userData`
 	);
-	const userDataJSON = JSON.parse(userData);
+	const userDataJSON = JSON.parse(userData || {});
 	let email;
 	userDataJSON.UserAttributes.forEach((ele) => {
 		email = ele.Name === "email" ? ele.Value : "";
--- src/aws-config.js
+++ src/aws-config.js
@@ -1,3 +1,66 @@
+export const Amplifyconfig = {
+	API: {
+		endpoints: [
+			{
+				name: "ApplicantSubmission",
+
+				endpoint:
+					"https://g9yh14f7ve.execute-api.ap-south-1.amazonaws.com/Authorizeddev",
+			},
+		],
+	},
+
+	Auth: {
+		// REQUIRED - Amazon Cognito Region
+
+		region: "ap-south-1",
+
+		// OPTIONAL - Amazon Cognito Federated Identity Pool Region
+
+		// Required only if it's different from Amazon Cognito Region
+
+		identityPoolRegion: "ap-south-1",
+
+		// OPTIONAL - Amazon Cognito User Pool ID
+
+		userPoolId: "ap-south-1_T6Eai4D7M",
+
+		// OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+
+		userPoolWebClientId: "5hesaar1ubosqmd96f6kucqjiu",
+
+		// OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+
+		mandatorySignIn: false,
+
+		// OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
+
+		authenticationFlowType: "USER_SRP_AUTH",
+
+		// OPTIONAL - Manually set key value pairs that can be passed to Cognito Lambda Triggers
+
+		clientMetadata: { myCustomKey: "myCustomValue" },
+
+		// OPTIONAL - Hosted UI configuration
+
+		oauth: {
+			domain: "applicants.auth.ap-south-1.amazoncognito.com",
+
+			scope: ["email", "openid"],
+
+			redirectSignIn: "https://master.d3n4gc65xuvvqw.amplifyapp.com/",
+
+			logoutUri: "https://master.d3n4gc65xuvvqw.amplifyapp.com/",
+
+			redirectUri: "https://master.d3n4gc65xuvvqw.amplifyapp.com/",
+
+			redirectSignOut: "https://master.d3n4gc65xuvvqw.amplifyapp.com/",
+
+			responseType: "token", // or 'token', note that REFRESH token will only be generated when the responseType is code
+		},
+	},
+};
+
 export const config = {
 	apiGateway: {
 		REGION: "ap-south-1",
