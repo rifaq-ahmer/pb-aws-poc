cfee4a7e7f56e628c76e6bc1be90a8d561658127
Rifaq.Shaikh <rifaq.shaikh@extentia.com>
2021-10-12 12:52:09 +0530

    added JSON.parse

--- src/App.js
+++ src/App.js
@@ -102,7 +102,7 @@ function App() {
 	const userData = localStorage.getItem(
 		`CognitoIdentityServiceProvider.${APP_CLIENT_ID}.${userName}.userData`
 	);
-	const userDataJSON = JSON.parse(JSON.stringify(userData || {}));
+	const userDataJSON = JSON.parse(userData);
 	let email;
 	userDataJSON.UserAttributes.forEach((ele) => {
 		email = ele.Name === "email" ? ele.Value : "";
